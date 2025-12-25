79925c900613902ed3974955ad9c77a4b8d01eaf
LearnDevbiswjit <learndev.biswajit@gmail.com>
2025-12-25 16:15:49 +0530

    fix css import case for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 
-
- 
-
 // ✅ Service Worker ONLY in production
-if (
-  import.meta.env.PROD &&
-  'serviceWorker' in navigator
-) {
+if (import.meta.env.PROD && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
   })
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
